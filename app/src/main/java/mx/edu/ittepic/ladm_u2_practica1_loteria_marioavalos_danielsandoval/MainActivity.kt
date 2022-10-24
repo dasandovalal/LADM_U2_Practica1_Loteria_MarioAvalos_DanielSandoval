189b6bf074dc6b6a60f8179dc95002e649c71fe6
Mario Alberto Avalos <maalavalosro@ittepic.edu.mx>
2022-10-23 20:06:34 -0600

    Proyecto Terminado

--- app/src/main/java/mx/edu/ittepic/ladm_u2_practica1_loteria_marioavalos_danielsandoval/MainActivity.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u2_practica1_loteria_marioavalos_danielsandoval/MainActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
